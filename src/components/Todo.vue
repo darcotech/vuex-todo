d73631933e47b507d3fedc4754f013e76e165989
Michael Wanyoike <micwanyoike@gmail.com>
2018-10-29 19:21:55 +0300

    Restylized todo form

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,23 +1,22 @@
 <template>
   <div class="todo">
-    <b-row>
-      <b-col>
-        <b-form inline>
-          <label class="sr-only" for="item">Item</label>
-          <b-form-input
+    <b-form class="row">
+      <b-col cols="10" style="paddingLeft:0">
+        <b-form-input
             id="item"
-            class="w-75 p-3 mb-1"
+            class="w-100"
             name="item"
             placeholder="Enter Item"
             required>
           </b-form-input>
-          <b-button variant="primary">Add Task</b-button>
-        </b-form>
       </b-col>
-    </b-row>
+      <b-col cols="2">
+        <b-button variant="primary">Add Task</b-button>
+      </b-col>
+    </b-form>
     <b-row>
       <b-col>
-        <b-list-group class="w-75 mb-1">
+        <b-list-group class=" mb-1">
           <b-list-group-item class="row">
             <b-col cols="1">
               <b-form-checkbox> </b-form-checkbox>
@@ -43,10 +42,10 @@
 form {
   margin-bottom: 25px;
 }
-input {
-  margin-right: 25px;
-}
 .list-group-item {
   display: flex;
+}
+.list-group-item:hover{
+  background-color: aliceblue;
 }
 </style>
